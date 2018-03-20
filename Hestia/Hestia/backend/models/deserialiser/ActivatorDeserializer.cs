8b9d427f89fdf61551f62b90f21461453b35ea98
Jur Kroeze <SinasAppel@users.noreply.github.com>
2018-03-20 23:02:25 +0100

    Activator and Device deserializers

---
+++ Hestia/Hestia/backend/models/deserialiser/ActivatorDeserializer.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace Hestia.backend.models.des
+{
+    /**
+     * Helper class that deserializes a json object into an Activator object
+     */
+    class ActivatorDeserializer
+    {
+        public ActivatorDeserializer() { }
+
+        public Activator deserializeJson(string jsonData)
+        {
+            return JsonConvert.DeserializeObject<Activator>(jsonData);
+        }
+
+    }
+}
