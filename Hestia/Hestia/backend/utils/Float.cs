7d601cfecb219f2f7ffda6cc91961f0755dbdc85
Zino <z.holwerda@ho2o.nl>
2018-03-23 23:49:20 +0100

    Made float class, extends from object for use a generic object

---
+++ Hestia/Hestia/backend/utils/Float.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+namespace Hestia.backend.utils
+{
+    class Float : Object
+    {
+        private float value;
+        public float Value { get; set; }
+
+        public Float (float value)
+        {
+            this.value = value;
+        }
+    }
+}
