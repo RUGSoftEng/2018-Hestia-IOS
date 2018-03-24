7c2b8c55cc7ad36b9fc352bfe0bcc9c27b408d90
daangroot <groot.d@hotmail.nl>
2018-03-24 20:27:32 +0100

    Update time tracking.

---
+++ Hestia/Hestia/backend/models/deserialisers/DeviceDeserializer.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Hestia.backend.models.deserialisers
+{
+    /**
+     * Helper class that deserializes a json object into a Device object
+     */
+    class DeviceDeserializer
+    {
+        public DeviceDeserializer() { }
+
+        public Device deserializeDevice(string jsonData)
+        {
+            return JsonConvert.DeserializeObject<Device>(jsonData);
+        }
+    }
+}
