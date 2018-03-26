64aa8fc383d3f72af7b575b5dcca6f3e6a2973b8
Jur Kroeze <SinasAppel@users.noreply.github.com>
2018-03-26 22:50:42 +0200

    RequiredInfoDeserializer not needed, methods notation changed

--- Hestia/Hestia/backend/models/deserializers/ActivatorDeserializer.cs
+++ Hestia/Hestia/backend/models/deserializers/ActivatorDeserializer.cs
@@ -7,7 +7,7 @@ namespace Hestia.backend.models.deserializers
      */
     class ActivatorDeserializer
     {
-        public Activator deserialize(JToken jT)
+        public Activator Deserialize(JToken jT)
         {
             // get the activatorId, rank and name
             string id = jT.Value<string>("activatorId");
