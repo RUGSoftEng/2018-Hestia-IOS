810e221a6ab60d3051db69dba166c3c2f3f5582b
T d B <nacloverload@Ts-iMac.local>
2018-04-17 13:52:31 -0700

    implemented activator screen.

---
+++ Hestia/Hestia/DevicesScreen/UITableViewActivators.cs
@@ -0,0 +1,39 @@
+using Foundation;
+using System;
+using UIKit;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Collections;
+
+using Hestia.DevicesScreen.resources;
+using Hestia.backend;
+using Hestia.backend.models;
+
+namespace Hestia.DevicesScreen
+{
+    public partial class UITableViewActivators : UITableViewController
+    {
+        public UITableView table;
+        public Device device;
+        public Device Device { get; set; }
+
+        public UITableViewActivators (IntPtr handle) : base (handle)
+        {
+        }
+		public override void ViewDidLoad()
+		{
+            base.ViewDidLoad();
+            Title = device.Name;
+            table = new UITableView(View.Bounds)
+            {
+
+                // Contains methods that describe behavior of table
+                Source = new TableSourceActivators(device)
+            };
+
+            // Add the table to the view
+            Add(table); 
+
+		}
+	}
+}
