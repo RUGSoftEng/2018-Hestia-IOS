3bf03e4468b9d0d897c379a68cdb402701d2fffc
J.J. Mendez Torrero <s3542416@ai178140.ai.rug.nl>
2018-04-26 15:59:04 +0200

    Server List screen

---
+++ Hestia/Hestia/frontend/Firebase/TableSourceServerList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using UIKit;
+using Foundation;
+using Hestia.DevicesScreen;
+using Hestia.DevicesScreen.resources;
+using System.Drawing;
+using System.Collections;
+using Hestia.backend;
+using Hestia.backend.models;
+
+namespace Hestia
+{
+    public class TableSourceServerList : UITableViewSource
+    {
+        UITableViewServerList owner;
+        // The kind of cell that is used in the table (set in Storyboard)
+        string CellIdentifier = "servercell";
+
+        public TableSourceServerList(List<FireBaseServer> item, UITableViewServerList owner)
+        {
+            Globals.FirebaseServers = item;
+            this.owner = owner;
+        }
+
+		public override nint NumberOfSections(UITableView tableView)
+		{
+            return 1;
+		}
+
+		public override nint RowsInSection(UITableView tableview, nint section)
+		{
+            return Globals.FirebaseServers.Count;
+		}
+
+		public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+		{
+            UITableViewCell cell = tableView.CellAt(indexPath);
+
+            if (Globals.FirebaseServers[indexPath.Row].Selected == false)
+            {
+                cell.Accessory = UITableViewCellAccessory.Checkmark;
+                Globals.FirebaseServers[indexPath.Row].Selected = true;
+            }
+            else
+            {
+                cell.Accessory = UITableViewCellAccessory.None;
+                Globals.FirebaseServers[indexPath.Row].Selected = false;
+            }
+            tableView.DeselectRow(indexPath, true);
+		}
+
+
+		
+		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+		{
+            // request a recycled cell to save memory
+            UITableViewCell cell = tableView.DequeueReusableCell(CellIdentifier);
+            // if there are no cells to reuse, create a new one
+            if (cell == null)
+            {
+                // Generate a default table cell
+                cell = new UITableViewCell(UITableViewCellStyle.Default, CellIdentifier);
+            }
+            // The text to display on the cell is the plugin name
+            cell.TextLabel.Text = Globals.FirebaseServers[indexPath.Row].Interactor.getIP();
+
+            return cell;
+		}
+	}
+}
---
+++ Hestia/Hestia/frontend/Firebase/UITableViewServerList.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using UIKit;
+using Foundation;
+using Hestia.DevicesScreen;
+using Hestia.DevicesScreen.resources;
+using System.Drawing;
+using System.Collections;
+using Hestia.backend;
+using Hestia.backend.models;
+using Hestia.backend.utils;
+
+namespace Hestia
+{
+    public partial class UITableViewServerList : UITableViewController
+    {
+        
+        List<FireBaseServer> serverList = new List<FireBaseServer>()
+        {
+            new FireBaseServer(false, new ServerInteractor(new NetworkHandler("94.212.164.28", 8000))),
+            new FireBaseServer(false, new ServerInteractor(new NetworkHandler("94.212.164.26", 8000))),
+            new FireBaseServer(false, new ServerInteractor(new NetworkHandler("94.212.164.27", 8000))),
+        };
+
+        public UITableViewServerList (IntPtr handle) : base (handle)
+        {
+        }
+
+		public override void ViewDidLoad()
+		{
+            base.ViewDidLoad();
+            Globals.LocalLogin = false;
+            TableView.Source = new TableSourceServerList(serverList, this);
+
+		}
+
+
+
+	}
+}
