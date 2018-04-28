a97a298d97e01037df423ddb665fe902c90044e4
daangroot <groot.d@hotmail.nl>
2018-04-28 20:24:18 +0200

    Added networkhandler unit tests.

--- Hestia/UnitTestHestia/backend/NetworkHandlerTest.cs
+++ Hestia/UnitTestHestia/backend/NetworkHandlerTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Hestia.backend;
+using Hestia.Resources;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 
 namespace UnitTestHestia.backend
@@ -6,23 +8,41 @@ namespace UnitTestHestia.backend
     [TestFixture]
     public class NetworkHandlerTest
     {
-        [Test]
-        public void Pass()
+        private NetworkHandler dummyNetworkHandler;
+        private string dummyIp = "0.0.0.0";
+        private int dummyPort = 1000;
+
+        [SetUp]
+        public void SetUpNetworkHandler()
         {
-            Assert.True(true);
+            dummyNetworkHandler = new NetworkHandler(dummyIp, dummyPort);
+            Assert.IsNotNull(dummyNetworkHandler);
         }
 
         [Test]
-        public void Fail()
+        public void SetAndGetIpTest()
         {
-            Assert.False(true);
+            Assert.AreEqual(dummyIp, dummyNetworkHandler.Ip);
+            string newIp = "94.212.164.28";
+            dummyNetworkHandler.Ip = newIp;
+            Assert.AreEqual(newIp, dummyNetworkHandler.Ip);
         }
 
         [Test]
-        [Ignore("another time")]
-        public void Ignore()
+        public void SetAndGetPortTest()
         {
-            Assert.True(false);
+            Assert.AreEqual(dummyPort, dummyNetworkHandler.Port);
+            int newPort = 8000;
+            dummyNetworkHandler.Port = newPort;
+            Assert.AreEqual(newPort, dummyNetworkHandler.Port);
+        }
+
+        [Test]
+        public void ExecuteGetRequestTest()
+        {
+            string endpoint = strings.devicePath;
+            JToken response = dummyNetworkHandler.Get(endpoint);
+            Assert.IsTrue(JsonValidator.IsValidJson(response.ToString()));
         }
     }
 }
