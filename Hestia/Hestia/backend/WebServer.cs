d93b39a1f790f707d2bf95ca019d304f60cd659d
Zino Holwerda <z.holwerda@ho2o.nl>
2018-05-01 22:17:13 +0200

    Forgot the actual class name

--- Hestia/Hestia/backend/WebServer.cs
+++ Hestia/Hestia/backend/WebServer.cs
@@ -1,12 +1,12 @@
 ﻿namespace Hestia.backend
 {
-    public class FireBaseServer
+    public class WebServer
     {
         public ServerInteractor Interactor { get; set; }
         // This boolean is used to enable or disable certain in the selection screen.
         public bool Selected { set; get; }
 
-        public FireBaseServer(bool selected, ServerInteractor interactor)
+        public WebServer(bool selected, ServerInteractor interactor)
         {
             Interactor = interactor;
             Selected = selected;
