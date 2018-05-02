bb113d4c9778fa9a9bc079957309292300722034
Zino Holwerda <z.holwerda@ho2o.nl>
2018-05-02 14:37:11 +0200

    Added a way of using auth0 for front end.

---
+++ Hestia/Hestia.UnitTests/backend/authentication/Auth0Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hestia.UnitTests.backend.authentication
+{
+    [TestClass]
+    public class Auth0Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ Hestia/Hestia/backend/authentication/Auth0.cs
@@ -0,0 +1,19 @@
+﻿using Auth0.OidcClient;
+using UIKit;
+
+namespace Hestia.backend.authentication
+{
+    public class Auth0
+    {
+        public Auth0Client CreateAuthClient(UITableViewController uITableViewController)
+        {
+            var client = new Auth0Client(new Auth0ClientOptions
+            {
+                Domain = "hestio.eu.auth0.com",
+                ClientId = "_1wsVGXJbkEm8kwcueB5seyxm37E1rWl",
+                Controller = uITableViewController
+            });
+            return client;
+        }
+    }
+}
