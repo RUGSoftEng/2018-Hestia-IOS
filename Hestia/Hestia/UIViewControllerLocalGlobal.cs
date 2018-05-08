b5ef451232dc14d95ad919e9a0a7b409d9985bf5
Marc Fleurke <marcfleurke@gmail.com>
2018-05-08 11:45:38 -0700

    Linked new entrance screen to local login

--- Hestia/Hestia/UIViewControllerLocalGlobal.cs
+++ Hestia/Hestia/UIViewControllerLocalGlobal.cs
@@ -2,64 +2,104 @@ using Foundation;
 using System;
 using UIKit;
 using Hestia.backend.utils;
-using Hestia.backend.models;
 using Hestia.backend;
 using Hestia.Resources;
 using Hestia.DevicesScreen.resources;
 
+
 namespace Hestia
-{
+{   // This view controller belongs to the first window that can be shown on loading the app
+    // if no user defaults are present
     public partial class UIViewControllerLocalGlobal : UIViewController
     {
+        string defaultServerName;
+        string defaultIP;
+        string defaultPort;
+
         public UIViewControllerLocalGlobal (IntPtr handle) : base (handle)
         {
         }
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
+        }
+
+        void SetValuesAndSegueToDevicesMain()
+        {
+            Globals.LocalLogin = true;
+            Globals.ServerName = defaultServerName;
+            Globals.IP = defaultIP;
+            Globals.Port = int.Parse(defaultPort);
+            Globals.LocalServerinteractor = new ServerInteractor(new NetworkHandler(Globals.IP, Globals.Port));
+            Console.WriteLine("To devices main");
+            PerformSegue(strings.mainToDevicesMain, this);
+        }
+
+		public override void ViewDidAppear(bool animated)
+		{
+            base.ViewDidAppear(animated);
             bool validIp = false;
 
-            // Already anticipate local login
             NSUserDefaults userDefaults = NSUserDefaults.StandardUserDefaults;
-            string defaultServerName = userDefaults.StringForKey(Resources.strings.defaultsServerNameHestia);
-            string defaultIP = userDefaults.StringForKey(Resources.strings.defaultsIpHestia);
-            string defaultPort = userDefaults.StringForKey(Resources.strings.defaultsPortHestia);
-            bool defaultLocal = userDefaults.BoolForKey("defaultLocalHestia");
+            defaultServerName = userDefaults.StringForKey(strings.defaultsServerNameHestia);
+            defaultIP = userDefaults.StringForKey(strings.defaultsIpHestia);
+            defaultPort = userDefaults.StringForKey(strings.defaultsPortHestia);
+            string defaultLocal = userDefaults.StringForKey(strings.defaultsLocalHestia);
 
-            if (defaultLocal)
+            // Already anticipate local login
+            // Check if local serverinformation is present and correct
+            if (defaultServerName != null && defaultIP != null && defaultPort != null)
             {
+                validIp = PingServer.Check(defaultIP, int.Parse(defaultPort));
+            }
 
-                if (defaultServerName != null && defaultIP != null && defaultPort != null)
-                {
-                    validIp = PingServer.Check(defaultIP, int.Parse(defaultPort));
-                }
-
+            // Check if a last startup option is unknown
+            if (defaultLocal == null)
+            {
                 ToLocalButton.TouchUpInside += delegate (object sender, EventArgs e)
                 {
-                    Globals.LocalLogin = true;
+                    userDefaults.SetString(bool.TrueString, strings.defaultsLocalHestia);
+
                     if (validIp)
                     {
-                        Globals.ServerName = defaultServerName;
-                        Globals.IP = defaultIP;
-                        Globals.Port = int.Parse(defaultPort);
-                        ServerInteractor serverInteractor = new ServerInteractor(new NetworkHandler(Globals.IP, Globals.Port));
-                        Globals.LocalServerinteractor = serverInteractor;
-                        Console.WriteLine("To devices main");
-                        PerformSegue("mainToDevicesMain", this);
+                        SetValuesAndSegueToDevicesMain();
                     }
                     else
                     {
                         Console.WriteLine("To serverconnect");
-                        PerformSegue("mainToServerConnect", this);
-
+                        PerformSegue(strings.mainToServerConnect, this);
                     }
                 };
+
+                ToGlobalButton.TouchUpInside += delegate (object sender, EventArgs e)
+                {
+                    Globals.LocalLogin = false;
+                    userDefaults.SetString(bool.FalseString, strings.defaultsLocalHestia);
+
+                    Console.WriteLine("To auth0login");
+                    //PerformSegue(strings.mainToServerConnect, this);
+                };
+            }
+            // Last login was local
+            else if (bool.Parse(defaultLocal))
+            {
+                if (validIp)
+                {
+                    SetValuesAndSegueToDevicesMain();
+                }
+                else
+                {
+                    Console.WriteLine("To serverconnect");
+                    PerformSegue(strings.mainToServerConnect, this);
+                }
             }
             else // Previous time global login
             {
+                // Should ultimately go direct to multiple server screen
                 Console.WriteLine("To auth0login");
-                PerformSegue("mainToAuth0", this);
+                PerformSegue(strings.mainToAuth0, this);
             }
-        }
-    }
+        
+		}
+	}
 }
