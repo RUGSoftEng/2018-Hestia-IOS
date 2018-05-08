66e0d80472ea4633e2028cf3a4db9b46f976fbb7
Zino Holwerda <z.holwerda@ho2o.nl>
2018-05-08 13:24:50 +0200

    Removed reduncies, now returns the delegate that contains a list of found services

--- Hestia/Hestia/backend/utils/server_discovery/AutoServerDicovery.cs
+++ Hestia/Hestia/backend/utils/server_discovery/AutoServerDicovery.cs
@@ -1,25 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Foundation;
-using UIKit;
+﻿using Foundation;
 
 namespace Hestia.backend.utils.server_discovery
 {
     class AutoServerDicovery
     {
-        public ServerInteractor Discover()
+        public ServerDelegate Discover()
         {
             // Setup of the service browser.
             NSNetServiceBrowser browser = new NSNetServiceBrowser();
             ServerDelegate @delegate = new ServerDelegate();
             browser.Init();
-            browser.Delegate = (INSNetServiceBrowserDelegate) @delegate;
-            
+            browser.Delegate = @delegate;
+
             browser.SearchForServices("_hestia._tcp", @"local");
-            return null;
+            return @delegate;
         }
     }
 }
