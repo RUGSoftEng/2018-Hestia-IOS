76527f5cbb944abe9db6a025151f3dbc2dce1a17
Zino Holwerda <z.holwerda@ho2o.nl>
2018-05-10 15:06:55 +0200

    Added the delegate function and a constant var

--- Hestia/Hestia/backend/authentication/Auth0Connector.cs
+++ Hestia/Hestia/backend/authentication/Auth0Connector.cs
@@ -6,6 +6,8 @@ namespace Hestia.backend.authentication
 {
     public class Auth0Connector
     {
+        private const string SCOPE = "openid";
+
         public static Auth0Client CreateAuthClient(UIViewController uIViewController)
         {
             Auth0Client client = new Auth0Client(new Auth0ClientOptions
@@ -13,7 +15,7 @@ namespace Hestia.backend.authentication
                 Domain = Resources.strings.domainAuth0,
                 ClientId = Resources.strings.clientId,
                 Controller = uIViewController,
-                Scope = "openid"
+                Scope = SCOPE
             });
             return client;
         }
