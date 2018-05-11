f653ba23ee9f3b5094138d37637775400103d28f
Zino Holwerda <z.holwerda@ho2o.nl>
2018-05-11 15:32:30 +0200

    Added methods for using the server discovery.

--- Hestia/Hestia/backend/utils/server_discovery/AutoServerDicovery.cs
+++ Hestia/Hestia/backend/utils/server_discovery/AutoServerDicovery.cs
@@ -2,18 +2,32 @@
 
 namespace Hestia.backend.utils.server_discovery
 {
+    /// <summary>
+    /// This class should be used to get the display the list of servers that are found by the service.
+    /// The delegate contains a list of services that are continuously update by this class untill Stop is called.
+    /// </summary>
     public class AutoServerDicovery
     {
-        public static ServerDelegate Discover()
+        public NSNetServiceBrowser Browser { get; set; }
+        public ServerDelegate Delegate { get; set; }
+
+        public AutoServerDicovery()
         {
             // Setup of the service browser.
-            NSNetServiceBrowser browser = new NSNetServiceBrowser();
-            ServerDelegate @delegate = new ServerDelegate();
-            browser.Init();
-            browser.Delegate = @delegate;
+            Browser = new NSNetServiceBrowser();
+            Delegate = new ServerDelegate();
+            Browser.Init();
+            Browser.Delegate = Delegate;            
+        }
 
-            browser.SearchForServices("_hestia._tcp", @"local");
-            return @delegate;
+        public void Discover()
+        {
+            Browser.SearchForServices("_hestia._tcp", @"local");
+        }
+
+        public void Stop()
+        {
+            Browser.Stop();
         }
     }
 }
