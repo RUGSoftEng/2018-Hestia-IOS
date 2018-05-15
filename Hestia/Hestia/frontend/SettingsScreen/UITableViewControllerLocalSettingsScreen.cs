980354b709fca133e01d63506ed25f5d5aa0a1dd
Marc Fleurke <marcfleurke@gmail.com>
2018-05-15 19:48:53 +0200

    Button to clear settings

--- Hestia/Hestia/frontend/SettingsScreen/UITableViewControllerLocalSettingsScreen.cs
+++ Hestia/Hestia/frontend/SettingsScreen/UITableViewControllerLocalSettingsScreen.cs
@@ -17,5 +17,21 @@ namespace Hestia
             base.ViewDidLoad();
             serverName.Text = Globals.ServerName;
         }
+
+        // Reset Defaults button
+		partial void UIButton87850_TouchUpInside(UIButton sender)
+		{
+			Globals.ResetAllUserDefaults();
+			DisplayConfirmationMessage();
+		}
+
+		private void DisplayConfirmationMessage()
+        {
+            string title = "User Defaults Reset";
+            string message = "All settings are cleared";
+            var okAlertController = UIAlertController.Create(title, message, UIAlertControllerStyle.Alert);
+            okAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+            PresentViewController(okAlertController, true, null);
+        }
 	}
 }
