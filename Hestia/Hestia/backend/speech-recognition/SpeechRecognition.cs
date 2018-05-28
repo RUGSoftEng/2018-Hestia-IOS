6d06ab08d431744c4afc308b23c049ecd7d35c9a
Zino Holwerda <z.holwerda@ho2o.nl>
2018-05-28 13:57:33 +0200

    Added the method call and summary

--- Hestia/Hestia/backend/speech-recognition/SpeechRecognition.cs
+++ Hestia/Hestia/backend/speech-recognition/SpeechRecognition.cs
@@ -12,6 +12,12 @@ namespace Hestia.backend.speech_recognition
 {
     class SpeechRecognition
     {
+        /// <summary>
+        /// This class can be used to form a speech dialog with the user.
+        /// It has several functions for retrieving the voice and excuting
+        /// commands based on what was said.
+        /// </summary>
+
         private AVAudioEngine AudioEngine = new AVAudioEngine();
         private SFSpeechRecognizer SpeechRecognizer = new SFSpeechRecognizer();
         private SFSpeechAudioBufferRecognitionRequest LiveSpeechRequest = new SFSpeechAudioBufferRecognitionRequest();
@@ -68,47 +74,40 @@ namespace Hestia.backend.speech_recognition
                     return;
                 }
             }
-
-            // Setup audio session
+            
             var node = AudioEngine.InputNode;
             var recordingFormat = node.GetBusOutputFormat(0);
             node.InstallTapOnBus(0, 1024, recordingFormat, (AVAudioPcmBuffer buffer, AVAudioTime when) => {
-                // Append buffer to recognition request
                 LiveSpeechRequest.Append(buffer);
             });
-
-            // Start recording
+            
             AudioEngine.Prepare();
             NSError error;
             AudioEngine.StartAndReturnError(out error);
-
-            // Did recording start?
+            
             if (error != null)
             {
                 Console.WriteLine("Couldn't start recording.");
 		        return;
             }
-
-            // Play start sound
+            
             if (player.IsPlaying) player.Stop();
             player.Load("Sounds/siri_start.mp3");
             player.Play();
-
-            // Start recognition
+            
             RecognitionTask = SpeechRecognizer.GetRecognitionTask(LiveSpeechRequest, (SFSpeechRecognitionResult result, NSError err) =>
             {
-                // Was there an error?
                 if (err != null)
                 {
                     Console.WriteLine("Error while recording.");
                 }
                 else
                 {
-                    // Is this the final translation?
                     if (result.Final)
                     {
                         this.result = result.BestTranscription.FormattedString;
-                        this.finished = true;
+                        DecideAction();
+                        finished = true;
                     }
                 }
             });
