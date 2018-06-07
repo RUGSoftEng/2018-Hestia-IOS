10c1178b7a8bba4da94faa48af288367f2a47875
Marc Fleurke <marcfleurke@gmail.com>
2018-06-07 13:44:58 +0200

    Make Serverlist screen similar to devices main

--- Hestia/Hestia/frontend/Auth0/ServerSelectScreen/TableSourceServerList.cs
+++ Hestia/Hestia/frontend/Auth0/ServerSelectScreen/TableSourceServerList.cs
@@ -2,11 +2,9 @@ using System;
 using UIKit;
 using Foundation;
 using Hestia.DevicesScreen.resources;
-using Hestia.Auth0;
-
-using Hestia.backend;
 using Hestia.backend.exceptions;
-using Hestia.frontend;
+using Hestia.DevicesScreen.EditDevice;
+using Hestia.Resources;
 
 namespace Hestia.Auth0
 {
@@ -19,26 +17,22 @@ namespace Hestia.Auth0
             this.owner = owner;
         }
 
-		public override nint NumberOfSections(UITableView tableView)
-		{
-            return int.Parse(Resources.strings.defaultNumberOfSections);
-		}
+        public override nint NumberOfSections(UITableView tableView)
+        {
+            return int.Parse(strings.defaultNumberOfSections);
+        }
 
-		public override nint RowsInSection(UITableView tableview, nint section)
-		{
-            return Globals.Auth0Servers.Count+1;
-		}
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return Globals.Auth0Servers.Count;
+        }
 
         // Sets the checkmarks
-		public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
-		{
+        public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+        {
             UITableViewCell cell = tableView.CellAt(indexPath);
-            if(cell.TextLabel.Text == "Add a new server")
+            if (!owner.Editing)
             {
-                AddServerViewController serverView = owner.Storyboard.InstantiateViewController("AddServer") as AddServerViewController;
-                owner.NavigationController.PushViewController(serverView, true);
-            }
-            else{
                 if (Globals.Auth0Servers[indexPath.Row].Selected == false)
                 {
                     cell.Accessory = UITableViewCellAccessory.Checkmark;
@@ -48,22 +42,33 @@ namespace Hestia.Auth0
                 {
                     cell.Accessory = UITableViewCellAccessory.None;
                     Globals.Auth0Servers[indexPath.Row].Selected = false;
-                } 
+                }
+
+            }
+            // Go to edit name window for non-insert cells
+            else
+            {
+                UIViewControllerEditDeviceName editViewController = new UIViewControllerEditDeviceName(this.owner);
+                editViewController.device = GetSectionRow(indexPath);
+                owner.NavigationController.PushViewController(editViewController, true);
             }
 
             tableView.DeselectRow(indexPath, true);
-		}
-        		
-		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
-		{
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
             // request a recycled cell to save memory
-            UITableViewCell cell = tableView.DequeueReusableCell(Resources.strings.serverCell);
+            UITableViewCell cell = tableView.DequeueReusableCell(strings.serverCell);
             // if there are no cells to reuse, create a new one
             if (cell == null)
             {
                 // Generate a default table cell
-                cell = new UITableViewCell(UITableViewCellStyle.Default, Resources.strings.serverCell);
+                cell = new UITableViewCell(UITableViewCellStyle.Default, strings.serverCell);
             }
+
+            cell.EditingAccessory = UITableViewCellAccessory.DisclosureIndicator;
+
             // The text to display on the cell is the server info
             if (indexPath.Row < Globals.Auth0Servers.Count)
             {
@@ -78,12 +83,84 @@ namespace Hestia.Auth0
                     cell.Accessory = UITableViewCellAccessory.Checkmark;
                 }
             }
-            else if(indexPath.Row >= Globals.Auth0Servers.Count)
+            else if (indexPath.Row >= Globals.Auth0Servers.Count)
             {
                 cell.TextLabel.Text = "Add a new server";
             }
 
             return cell;
-		}
-	}
+        }
+
+        /// <summary>
+        /// Defines what happens when the delete action is confirmed. 
+        /// Then the device is removed from the server and the list.
+        /// </summary>
+        public override void CommitEditingStyle(UITableView tableView, UITableViewCellEditingStyle editingStyle, Foundation.NSIndexPath indexPath)
+        {
+
+            var deviceInRow = serverDevices[indexPath.Section][indexPath.Row];
+            var deviceServerInteractor = deviceInRow.ServerInteractor;
+            try
+            {
+                deviceServerInteractor.RemoveDevice(deviceInRow);
+            }
+            catch (ServerInteractionException ex)
+            {
+                Console.WriteLine("Exception while removing device. (Bug in server: exception is always thrown)");
+                Console.Out.WriteLine(ex);
+            }
+
+            //Remove device from list that is shown in the TableView
+            RemoveDeviceAt(indexPath);
+
+            // Delete the row from the table
+            tableView.DeleteRows(new NSIndexPath[] { indexPath }, UITableViewRowAnimation.Fade);
+        }
+
+
+
+        /// <summary>
+        /// The action that is performed when the green plus icon is tapped to add a new device.
+        /// It segues to the add device screen in case of local login or to the server select screen
+        /// in case of global login, to choose the server to add the device to.
+        /// <see cref="ViewControllerChooseServer"/> 
+        /// <see cref="UITableViewControllerAddDevice"/>
+        /// </summary>
+        public void InsertAction()
+        {
+            AddServerViewController serverView = owner.Storyboard.InstantiateViewController("AddServer") as AddServerViewController;
+            owner.NavigationController.PushViewController(serverView, true);
+        }
+
+        /// <summary>
+        /// The rows cannot be edited in normal mode, but they can in editing mode.
+        /// That is: they can be deleted.
+        /// </summary>
+        /// <returns><c>true</c>, if row can be edited <c>false</c> otherwise.</returns>
+        public override bool CanEditRow(UITableView tableView, NSIndexPath indexPath)
+        {
+            if (tableView.Editing)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Is called after a press on edit button. The microphone icon is changed to an insertion icon.
+        /// </summary>
+        public void WillBeginTableEditing(UITableView tableView)
+        {
+            owner.ReloadButtons(true);
+        }
+
+        /// <summary>
+        /// Is called after a press on done button. The insertion icon is changed back to microphone icon.
+        /// </summary>
+        public void DidFinishTableEditing(UITableView tableView)
+        {
+            owner.ReloadButtons(false);
+        }
+    }
 }
+
--- Hestia/Hestia/frontend/Auth0/ServerSelectScreen/ViewControllerServerList.cs
+++ Hestia/Hestia/frontend/Auth0/ServerSelectScreen/ViewControllerServerList.cs
@@ -7,6 +7,11 @@ using Hestia.backend.exceptions;
 using Hestia.DevicesScreen.resources;
 using Hestia.frontend;
 
+using CoreGraphics;
+using Hestia.backend.models;
+using Hestia.Resources;
+using System.Collections.Generic;
+
 namespace Hestia
 {
     /// <summary>
@@ -16,6 +21,16 @@ namespace Hestia
     /// </summary>
     public partial class ViewControllerServerList : UITableViewController
     {
+        const int TableViewFooterHeight = 50;
+        const int IconDimension = 50;
+        const int Padding = 15;
+        nfloat bottomOfView;
+
+        // Cancel button in top left 
+        UIBarButtonItem cancel;
+        // Edit button in top right (is shown initially)
+        UIBarButtonItem edit;
+        // Edit button in top right (appears in edit mode)lly)
         UIBarButtonItem done;
 
         public ViewControllerServerList(IntPtr handle) : base(handle)
@@ -25,8 +40,33 @@ namespace Hestia
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            Title = Resources.strings.selectServerTitle;
+            Title = strings.selectServerTitle;
             TableView.Source = new TableSourceServerList(this);
+
+            // Fix the bottom position of the view, such that icons appear at same place when reloaded.
+            bottomOfView = TableView.Bounds.Bottom;
+
+            TableView.TableFooterView = GetTableViewFooter();
+           // RefreshServerList();
+            // To be able to tap a row in editing mode for changing name
+            TableView.AllowsSelectionDuringEditing = true;
+
+            edit = new UIBarButtonItem(UIBarButtonSystemItem.Edit, (s, e) => {
+                SetEditingState();
+            });
+
+            done = new UIBarButtonItem(UIBarButtonSystemItem.Done, (s, e) => {
+                CancelEditingState();
+            });
+
+            //Pull to refresh
+            RefreshControl = new UIRefreshControl();
+            RefreshControl.ValueChanged += RefreshTable;
+            TableView.Add(RefreshControl);
+
+            // Set right button initially to edit 
+            NavigationItem.LeftBarButtonItem = edit;
+            NavigationItem.RightBarButtonItem = cancel;
         }
 
         /// <summary>
@@ -36,6 +76,7 @@ namespace Hestia
         public override void ViewDidAppear(bool animated)
         {
             base.ViewDidAppear(animated);
+            RefreshServerList();
             TableView.ReloadData();
             // The done button that loads the devicesMainScreen with the selected servers
             done = new UIBarButtonItem(UIBarButtonSystemItem.Done, (s, e) => {
@@ -61,6 +102,7 @@ namespace Hestia
             {
                 SetCancelButtton(PresentingViewController);
             }
+            ReloadButtons(TableView.Editing);
         }
 
         /// <summary>
@@ -73,7 +115,7 @@ namespace Hestia
         public void SetCancelButtton(UIViewController uIViewController)
         {
             // Cancel button to go back to local/global screen
-            UIBarButtonItem cancel = new UIBarButtonItem(UIBarButtonSystemItem.Cancel, (sender, eventArguments) => {
+                cancel = new UIBarButtonItem(UIBarButtonSystemItem.Cancel, (sender, eventArguments) => {
                 if (uIViewController is UIViewControllerLocalGlobal)
                 {
                     DismissViewController(true, null);
@@ -110,5 +152,134 @@ namespace Hestia
             }
             return true;
         }
+
+        /// <summary>
+        /// This methods is called if the done button is touched. The button is changed to display "edit" and the 
+        /// list changes to normal mode (the delete icons disappear).
+        /// See, <see cref="TableSourceDevicesMain.DidFinishTableEditing(UITableView)"/>
+        /// </summary>
+        public void CancelEditingState()
+        {
+            TableView.SetEditing(false, true);
+            NavigationItem.LeftBarButtonItem = edit;
+            ((TableSourceServerList)TableView.Source).DidFinishTableEditing(TableView);
+        }
+
+
+        /// <summary>
+        /// This methods is called if the edit button is touched. The button is changed to display "done" and the 
+        /// list changes to editing mode (the delete icons appear).
+        /// See, <see cref="TableSourceDevicesMain.WillBeginTableEditing(UITableView))"/>
+        /// </summary>
+        public void SetEditingState()
+        {
+            ((TableSourceServerList)TableView.Source).WillBeginTableEditing(TableView);
+            TableView.SetEditing(true, true);
+            NavigationItem.LeftBarButtonItem = done;
+        }
+
+        /// <summary>
+        /// This method should be called if the list of devices should be updated from the server. 
+        /// This method is called for example when pull-to-refresh is performed.
+        /// </summary>
+        public void RefreshServerList()
+        {
+            try
+            {
+                List<HestiaServer> servers = Globals.HestiaWebServerInteractor.GetServers();
+                Globals.Auth0Servers = servers;
+            }
+            catch(ServerInteractionException ex)
+            {
+                Console.WriteLine("Exception while getting servers");
+                Console.WriteLine(ex);
+                WarningMessage.Display("Exception whle getting server", "Could not get the server information about local server from Auth0 server.", this);
+            }
+
+            TableView.Source = new TableSourceServerList(this);
+        }
+
+        void HandleException(TableSourceServerList source, ServerInteractionException ex)
+        {
+            Console.WriteLine("Exception while getting devices from local server");
+            Console.WriteLine(ex);
+            WarningMessage.Display("Could not refresh devices", "Exception while getting devices from local server", this);
+           // source.serverDevices = new List<List<Device>>();
+            TableView.ReloadData();
+        }
+
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+            RemoveButtons();
+        }
+
+        void RemoveButtons()
+        {
+            foreach (UIView view in ParentViewController.View.Subviews)
+            {
+                if (view is UIButton)
+                {
+                    view.RemoveFromSuperview();
+                }
+            }
+        }
+
+        public void ReloadButtons(bool isEditing)
+        {
+            RemoveButtons();
+            // Voice control / add device button
+            UIButton button = new UIButton(UIButtonType.System);
+            button.Frame = new CGRect(TableView.Bounds.Width - IconDimension - Padding, bottomOfView - TableViewFooterHeight - Padding, IconDimension, IconDimension);
+            if (isEditing)
+            {
+                button.SetBackgroundImage(UIImage.FromBundle(strings.addDeviceIcon), UIControlState.Normal);
+            }
+            else
+            {
+                button.SetBackgroundImage(UIImage.FromBundle(strings.voiceControlIcon), UIControlState.Normal);
+            }
+
+
+            button.TouchUpInside += (object sender, EventArgs e) =>
+            {
+                if (isEditing)
+                {   // segue to add device
+                    ((TableSourceServerList)TableView.Source).InsertAction();
+                }
+                else
+                {
+                    // segue
+                }
+            };
+
+
+            ParentViewController.View.AddSubview(button);
+        }
+
+        /// <summary>
+        /// The footer has enough free space to shown the complete microphone/insertion icons 
+        /// if the user scrolls to the bottom of the page.
+        /// </summary>
+        /// <returns>The table view footer.</returns>
+        public UIView GetTableViewFooter()
+        {
+            UIView view = new UIView(new CGRect(0, 0, TableView.Bounds.Width, TableViewFooterHeight));
+            return view;
+        }
+
+        /// <summary>
+        /// Method for Pull to refresh
+        /// </summary>
+        void RefreshTable(object sender, EventArgs e)
+        {
+            RefreshControl.BeginRefreshing();
+            RefreshServerList();
+            RefreshControl.EndRefreshing();
+        }
+
+
     }
 }
+
