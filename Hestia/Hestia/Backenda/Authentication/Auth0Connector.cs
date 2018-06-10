d8fd6a852d801fde7c6da53826af54ca73e0f2ac
daangroot <groot.d@hotmail.nl>
2018-06-10 22:33:06 +0200

    Rename folder.

---
+++ Hestia/Hestia/Backenda/Authentication/Auth0Connector.cs
@@ -0,0 +1,19 @@
+﻿using Auth0.OidcClient;
+using UIKit;
+
+namespace Hestia.Backend.Authentication
+{
+    public static class Auth0Connector
+    {
+        public static Auth0Client CreateAuthClient(UIViewController uIViewController)
+        {
+            Auth0Client client = new Auth0Client(new Auth0ClientOptions
+            {
+                Domain = Resources.strings.domainAuth0,
+                ClientId = Resources.strings.clientId,
+                Controller = uIViewController
+            });
+            return client;
+        }
+    }
+}
