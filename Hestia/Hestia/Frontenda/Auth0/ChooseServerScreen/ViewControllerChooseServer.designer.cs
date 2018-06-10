d0eb130a1a3ace7d1fe92733731e33cd4e080e1d
daangroot <groot.d@hotmail.nl>
2018-06-10 22:31:54 +0200

    Rename folder.

---
+++ Hestia/Hestia/Frontenda/Auth0/ChooseServerScreen/ViewControllerChooseServer.designer.cs
@@ -0,0 +1,21 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace Hestia.Frontend.Auth0.ChooseServerScreen
+{
+    [Register ("ViewControllerChooseServer")]
+    partial class ViewControllerChooseServer
+    {
+        void ReleaseDesignerOutlets ()
+        {
+        }
+    }
+}
---
+++ Hestia/Hestia/Frontenda/DevicesScreen/AddDeviceScreen/UITableViewControllerAddDeviceDevice.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using UIKit;
+
+namespace Hestia.Frontend.DevicesScreen.AddDeviceScreen
+{
+    public partial class UITableViewControllerAddDeviceDevice : UITableViewController
+    {
+        public List<string> plugins;
+        public string collection;
+
+        public UITableViewControllerAddDeviceDevice (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Contains methods that describe behavior of table
+            Devices.Source = new TableSourceAddDeviceDevice(plugins, collection, this);
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+            // Cancel button to go back to Devices main screen
+            UIBarButtonItem cancel = new UIBarButtonItem(UIBarButtonSystemItem.Cancel, (sender, eventArguments) =>
+            {
+                NavigationController.PopToRootViewController(true);
+            });
+            NavigationItem.RightBarButtonItem = cancel;
+        }
+    }
+}
