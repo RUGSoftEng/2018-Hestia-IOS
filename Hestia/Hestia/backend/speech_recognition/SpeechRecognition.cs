5ccaae605ef9475ab5b71e15c2c5e6fbfb06bc59
M. Fleurke <marcfleurke@gmail.com>
2018-06-11 14:32:50 +0200

    added reloaddata

--- Hestia/Hestia/backend/speech_recognition/SpeechRecognition.cs
+++ Hestia/Hestia/backend/speech_recognition/SpeechRecognition.cs
@@ -78,7 +78,7 @@ namespace Hestia.backend.speech_recognition
                 if (err != null)
                 {
                     Console.WriteLine(strings.speechRecordError);
-                    viewController.ProcessSpeech(null);
+                    viewController.ProcessSpeech(" ");
                 }
                 else
                 {
